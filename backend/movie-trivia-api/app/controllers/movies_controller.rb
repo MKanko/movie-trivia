04921c25a6664aef567bb908ed27aada79a0a26b
“Mark <mkanko66@gmail.com>
2019-10-29 19:05:52 -0700

    stub out index, new, create, show action and strong params method in MoviesController

--- backend/movie-trivia-api/app/controllers/movies_controller.rb
+++ backend/movie-trivia-api/app/controllers/movies_controller.rb
@@ -1,2 +1,23 @@
 class MoviesController < ApplicationController
+
+    def index
+        movie = Movie.all
+    end 
+
+    def new
+        movie = Movie.new
+    end
+
+    def create
+        movie = Movie.new(movie_params)
+    end
+
+    def show
+        movie = Movie.find_by(:id params[:movie_id])
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:title)
 end
