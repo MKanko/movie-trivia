662df80b5389615abc49c911da79b069a3dcafad
“Mark <mkanko66@gmail.com>
2019-10-29 17:52:49 -0700

    stub out new, create, show actions in UsersController

--- backend/movie-trivia-api/app/controllers/users_controller.rb
+++ backend/movie-trivia-api/app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    def new
+        user = User.new
+    end 
+
+    def create
+        user = User.find_by(id: params[:user_id]) 
+    end 
+
+    def show 
+        user = User.find_by(id: params[:user_id])
+    end
+    
 end
