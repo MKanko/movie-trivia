85ca9e074eaa2e49eb4e96e05d157f06f5d03116
“Mark <mkanko66@gmail.com>
2019-10-29 18:14:30 -0700

    stub out new, create, show actions and strong params method in ResultsController

--- backend/movie-trivia-api/app/controllers/results_controller.rb
+++ backend/movie-trivia-api/app/controllers/results_controller.rb
@@ -1,2 +1,20 @@
 class ResultsController < ApplicationController
+
+    def new
+        result = Result.new
+    end 
+
+    def create
+        result = Result.new(result_params)
+    end 
+
+    def show
+        result = Result.find_by(:id params[:result_id])
+    end 
+
+    private
+
+    def result_params
+        params.require(:result).permit()
+    end
 end
