74614b78cf0934104fdf4e4c2aa8397de795e7d3
“Mark <mkanko66@gmail.com>
2019-10-29 18:03:00 -0700

    strong params user_params private method to UsersController

--- backend/movie-trivia-api/app/controllers/stats_controller.rb
+++ backend/movie-trivia-api/app/controllers/stats_controller.rb
@@ -1,2 +1,16 @@
 class StatsController < ApplicationController
+
+    def new
+        stat = Stat.new
+    end
+
+    def create
+        stat = Stat.new(stat_params)
+    end
+
+    private
+
+    def stat_params
+        params.require(:stat).permit(:movies, :quizzes, :scores)
+    end
 end
--- backend/movie-trivia-api/app/controllers/users_controller.rb
+++ backend/movie-trivia-api/app/controllers/users_controller.rb
@@ -5,11 +5,17 @@ class UsersController < ApplicationController
     end 
 
     def create
-        user = User.find_by(id: params[:user_id]) 
+        user = User.new(user_params) 
     end 
 
     def show 
         user = User.find_by(id: params[:user_id])
     end
-    
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username)
+    end 
+
 end
