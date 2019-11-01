fd9ab996827dd9af469b9f5bfaa8167e825fffc4
“Mark <mkanko66@gmail.com>
2019-10-31 19:21:16 -0700

    add column index/foreign key to stats table

--- backend/movie-trivia-api/db/schema.rb
+++ backend/movie-trivia-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_020710) do
+ActiveRecord::Schema.define(version: 2019_11_01_021932) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2019_11_01_020710) do
     t.integer "point_total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_stats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
