ac28a46f77f64a2d96e5c27cdfe93aabfd9751bf
“Mark <mkanko66@gmail.com>
2019-10-31 19:09:40 -0700

    add column index/foreign key users to quizzes table

--- backend/movie-trivia-api/db/schema.rb
+++ backend/movie-trivia-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_020024) do
+ActiveRecord::Schema.define(version: 2019_11_01_020710) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -41,7 +41,9 @@ ActiveRecord::Schema.define(version: 2019_11_01_020024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "movie_id"
+    t.integer "user_id"
     t.index ["movie_id"], name: "index_quizzes_on_movie_id"
+    t.index ["user_id"], name: "index_quizzes_on_user_id"
   end
 
   create_table "stats", force: :cascade do |t|
