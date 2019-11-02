5e9d0c9ce626997e1628f199fdd002666d82a780
“Mark <mkanko66@gmail.com>
2019-11-02 11:14:14 -0700

    add global varibles for url endpoints, add selector for main tag and addEventListener(DOMContentLoaded) to index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,9 @@
+const BASE_URL = "http://localhost:3000"
+const MOVIES_URL = `${BASE_URL}/movies`
+const QUIZZES_URL = `${BASE_URL}/quizzes`
 
+const main = document.getElementsByTagName('main')[0];
+
+document.addEventListener('DOMContentLoaded', function () {
+    getMovies();
+})
