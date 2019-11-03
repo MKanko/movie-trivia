1129bafe7c6ac34d44cf3e5a55838d76992e9260
“Mark <mkanko66@gmail.com>
2019-11-02 18:04:42 -0700

    add code for static method generateMovies with asynchronous (AJAX) fetch request to api

--- frontend/movie.js
+++ frontend/movie.js
@@ -2,6 +2,19 @@ class Movie {
     constructor(title, id) {
         this.title = title;
         this.id = id;
-    }   
+    }
+    
+    static generateMovies = () => {
+        fetch(MOVIES_URL)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                for (const element of json.data) {
+                    const movie = new Movie(element.attributes.title, element.id)
+                    movie.renderMovieLink()
+                }
+            })
+    }
 }
 
