cd0315ab7c31c9e4d8dbc814471ae650e1dc6420
“Mark <mkanko66@gmail.com>
2019-11-04 14:37:54 -0800

    edit question.js file to add class Question with constructor method

---
+++ frontend/question.js
@@ -0,0 +1,14 @@
+class Question {
+
+    constructor(question_number, question, answer_option_a, answer_option_b, 
+        answer_option_c, answer_option_d, correct_answer, answer_context) {
+            this.question_number = question_number
+            this.question = question
+            this.answer_option_a = answer_option_a
+            this.answer_option_b = answer_option_b
+            this.answer_option_c = answer_option_c
+            this.answer_option_d = answer_option_d
+            this.correct_answer = correct_answer
+            this.answer_context = answer_context 
+        }
+}
