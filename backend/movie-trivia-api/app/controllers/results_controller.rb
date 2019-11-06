3a259090afc77a454af1769ecbd96bc4d26e61b6
“Mark <mkanko66@gmail.com>
2019-11-05 17:01:38 -0800

    add new and create actions to ResultsController

--- backend/movie-trivia-api/app/controllers/results_controller.rb
+++ backend/movie-trivia-api/app/controllers/results_controller.rb
@@ -7,5 +7,17 @@ class ResultsController < ApplicationController
         }
         render json: ResultSerializer.new(results, options)
     end 
+
+    def new
+        result = Result.new
+    end 
+
+    def create
+        result = Result.new(user_params)
+        options = {
+            include: [:answers]
+        }
+        render json: ResultSerializer.new(result, options) 
+    end
     
 end
