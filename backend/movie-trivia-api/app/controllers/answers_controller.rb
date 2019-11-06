14fa79fd1ee298675ea90b641d68bb52d67d7ec2
“Mark <mkanko66@gmail.com>
2019-11-05 17:41:56 -0800

    add new and create actions in AnswersController

--- backend/movie-trivia-api/app/controllers/answers_controller.rb
+++ backend/movie-trivia-api/app/controllers/answers_controller.rb
@@ -1,2 +1,11 @@
 class AnswersController < ApplicationController
+
+    def new 
+        answer = Answer.new
+    end 
+
+    def create 
+        answer = Answer.new(answer_params)
+        render json: AnswerSerializer.new(answer)
+    end 
 end
