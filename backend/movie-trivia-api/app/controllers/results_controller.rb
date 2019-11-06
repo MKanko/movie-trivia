0d79f48bf867003cbcd93739e01ceb8d6b63d4f3
“Mark <mkanko66@gmail.com>
2019-11-05 16:58:30 -0800

    add index action to ResultsController

--- backend/movie-trivia-api/app/controllers/results_controller.rb
+++ backend/movie-trivia-api/app/controllers/results_controller.rb
@@ -1,2 +1,11 @@
 class ResultsController < ApplicationController
+
+    def index 
+        results = Result.all
+        options = {
+            include: [:answers]
+        }
+        render json: ResultSerializer.new(results, options)
+    end 
+    
 end
