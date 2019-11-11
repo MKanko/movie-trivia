51e4594ee58661481f67f8023be5eb254f99e315
“Mark <mkanko66@gmail.com>
2019-11-11 15:25:28 -0800

    remove unnecessary controller actions

--- backend/movie-trivia-api/app/controllers/stats_controller.rb
+++ backend/movie-trivia-api/app/controllers/stats_controller.rb
@@ -1,31 +1,9 @@
 class StatsController < ApplicationController
 
-    def new
-        stat = Stat.new
-    end
-
-    def create
-        stat = Stat.new(stat_params)
-    end
-
     def show 
         stat = Stat.find_by(id: params[:id])
 
         render json: StatSerializer.new(stat)
     end
-
-    def edit
-        stat = Stat.find(params[:id])
-    end
-
-    def update
-        stat = Stat.find(params[:id])
-        stat.update(stat_params)
-    end
-
-    private
-
-    def stat_params
-        params.require(:stat).permit(:movie_history, :quiz_history, :rating, :point_total)
-    end
+    
 end
