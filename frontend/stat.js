c85bcfe3281a9b97c61c81b6625a6df419294a5a
“Mark <mkanko66@gmail.com>
2019-11-10 16:26:36 -0800

    add id attribute to constructor, add static updateStat method, add div element to renderStat method and edit statCard and div appendChild

--- frontend/stat.js
+++ frontend/stat.js
@@ -1,24 +1,46 @@
 class Stat {
 
-    constructor(movie_history, quiz_history, rating, point_total) {
+    constructor(movie_history, quiz_history, rating, point_total, id) {
         this.movie_history = movie_history 
         this.quiz_history = quiz_history 
-        this. rating = rating 
-        this.point_total = point_total 
+        this.rating = rating 
+        this.point_total = point_total
+        this.id = id  
     }
 
-    static renderStat = () => {
+    static updateStat = () => {
         const statCard = document.querySelector('#stat-card')
+        const statId = statCard.dataset.statId
+
+        fetch(`${STATS_URL}/${statId}`)
+            .then(function(response) {
+                return response.json()
+            })
+            .then(function(json) {
+                const statData = json.data.attributes  
+                const stat = new Stat(statData.movie_history, statData.quiz_history, statData.rating, 
+                    statData.point_total, json.data.id)
+
+                stat.renderStat() 
+            })    
+                           
+            
+    }
+
+    renderStat() {
+        const statCard = document.querySelector('#stat-card')
+        const div = document.createElement('div')
         const h4 = document.createElement('h4')
         const h5 = document.createElement('h5')
 
-        h4.innerText = 'Your Game Stats'
+        h4.innerText = 'Your Total Points'
         h5.innerText = this.point_total
-        statCard.dataset.userId = this.user_id
+        statCard.dataset.statId = this.id
+        statCard.innerHTML = ''
 
-        statCard.appendChild(h4)
-        statCard.appendChild(h5)
+        div.appendChild(h4)
+        div.appendChild(h5)
+        statCard.appendChild(div)
     }
-
      
 }
