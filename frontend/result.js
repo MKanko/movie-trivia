282f39ee0c74fc59a8d251d3b512066643686b0b
“Mark <mkanko66@gmail.com>
2019-11-11 15:01:20 -0800

    change variable name for argument passed into renderCards helper method called in renderResult method in result.js

--- frontend/result.js
+++ frontend/result.js
@@ -9,9 +9,11 @@ class Result {
     static generateResult(json) {
         const result = new Result(json.data.attributes.title, json.data.attributes.point_value, 
             json.data.attributes.score, json.data.id)
+            
         const ansArr = json.included.map(function (element) {
-            return new Answer(element.attributes.question_number, element.attributes.question, element.attributes.selected_answer, 
-                element.attributes.correct_answer, element.attributes.answer_context)
+            return new Answer(element.attributes.question_number, element.attributes.question, 
+                element.attributes.selected_answer, element.attributes.correct_answer, 
+                element.attributes.answer_context)
         })
         result.renderResult(ansArr)
         
