6bfe23182900dbefeae00f48f237e870a7c09e07
“Mark <mkanko66@gmail.com>
2019-11-11 16:28:37 -0800

    add edits to renderCards function display sign out button, add signOut function helper.js

--- frontend/helper.js
+++ frontend/helper.js
@@ -1,23 +1,48 @@
 
-const renderCards = (score) => {
-
+const renderCards = (score) => {    
     const quizCard = document.querySelector('#quiz-card')
     const navCard = document.querySelector('#nav-card')
     const h5 = document.createElement('h5')
     const h4 = document.createElement('h4')
     const btn = document.createElement('button')
+    const h42 = document.createElement('h4')
+    const btn2 = document.createElement('button')
 
     h5.innerText = `Your score ${score} points`
     h4.innerText = 'Take another quiz'
     btn.innerText = 'Movies'
+    h42.innerText = 'Finished Playing'
+    btn2.innerText = 'Sign Out'
 
     quizCard.appendChild(h5)
     navCard.appendChild(h4)
-    navCard.appendChild(btn) 
+    navCard.appendChild(btn)
+    navCard.appendChild(h42)
+    navCard.appendChild(btn2) 
 
     btn.addEventListener('click', function() {
         quizCard.style.display = 'none'
         h5.remove()
         Movie.generateMovies()
     })
+
+    btn2.addEventListener('click', function () {
+        signOut()        
+    })
+
+    const signOut = () => {
+        container.innerText = ''
+        const userCard = document.querySelector('#user-card')
+        const statCard = document.querySelector('#stat-card')
+        const quizCard = document.querySelector('#quiz-card')
+        const navCard = document.querySelector('#nav-card')
+
+        userCard.style.display = 'none'
+        statCard.style.display = 'none'
+        quizCard.style.display = 'none'
+        navCard.style.display = 'none'
+
+        renderUserSignIn()
+    }
+
 }
